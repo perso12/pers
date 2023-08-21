9fcaf52c086cbbe85758309631fe7d1fae791020
Perso <95594975+perso12@users.noreply.github.com>
2023-08-21 02:48:48 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xb87bd8eefb9b2ab0a4a20d4209ac70f3d2b5c7e0";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
